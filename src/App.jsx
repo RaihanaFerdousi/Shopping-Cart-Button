0224766854438f6dd8efb08eb350b0085e3fd47e
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-12-18 11:27:07 +0600

    fixed boxshadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import { useState } from "react";
-import { FaMinus } from "react-icons/fa6";
-import { FiPlus } from "react-icons/fi";
+import { AiOutlineMinus } from "react-icons/ai";
+import { GoPlus } from "react-icons/go";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
 
 function App() {
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(35);
   const [animationClass, setAnimationClass] = useState("");
-  const [clickedButton, setClickedButton] = useState(null); // Added clickedButton state
+  const [clickedButton, setClickedButton] = useState(null);
 
   const handleIncrement = () => {
     setAnimationClass("animate-from-right");
-    setClickedButton("increment"); // Set clickedButton to "increment"
+    setClickedButton("increment");
     setQuantity(quantity + 1);
     setPrice(price + 35);
     resetStates();
@@ -21,7 +21,7 @@ function App() {
   const handleDecrement = () => {
     if (quantity > 1) {
       setAnimationClass("animate-from-left");
-      setClickedButton("decrement"); // Set clickedButton to "decrement"
+      setClickedButton("decrement");
       setQuantity(quantity - 1);
       setPrice(price - 35);
       resetStates();
@@ -31,8 +31,8 @@ function App() {
   const resetStates = () => {
     setTimeout(() => {
       setAnimationClass("");
-      setClickedButton(null); // Reset clickedButton
-    }, 200);
+      setClickedButton(null);
+    }, 80);
   };
 
   return (
@@ -42,7 +42,7 @@ function App() {
           <button
             onClick={handleDecrement}
             disabled={quantity <= 1}
-            className={`text-7xl ${
+            className={`text-6xl ${
               quantity <= 1
                 ? "text-gray-500 cursor-not-allowed"
                 : clickedButton === "decrement"
@@ -50,28 +50,28 @@ function App() {
                 : "text-white"
             }`}
           >
-            <FaMinus />
+            <AiOutlineMinus />
           </button>
           <h2
-            className="text-6xl text-white py-5 px-8 rounded-xl"
+            className="text-5xl text-white bg-[#32373c] py-5 px-8 rounded-xl"
             style={{
-              boxShadow: "inset 3px 3px 9px rgba(0, 0, 0, 0.5)",
+              boxShadow: "inset 0 4px 8px -2px #1f2226",
             }}
           >
             <span className={animationClass}>{quantity}</span>
           </h2>
           <button
             onClick={handleIncrement}
-            className={`text-7xl ${
+            className={`text-6xl font-thin ${
               clickedButton === "increment" ? "text-[#00F5C3]" : "text-white"
             }`}
           >
-            <FiPlus className="font-thin" />
+            <GoPlus />
           </button>
         </div>
         <div>
           <button
-            className="flex text-5xl bg-[#00F5C3] gap-3 px-6 py-5 rounded-lg"
+            className="flex text-5xl bg-[#00F5C3] text-[#3C4247] gap-3 px-6 py-5 rounded-lg"
             style={{
               boxShadow: "0 0 15px 5px rgba(0, 245, 195, 0.6)",
             }}
